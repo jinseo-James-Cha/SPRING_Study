155b3b7afc27d07ad1cdffbdd72a22fe76d8f724
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-08 12:49:36 +0900

    2021-06-08 morning study done

---
+++ chap02-component-scan-lecture-source/src/com/greedy/section01/javaconfig/Application1.java
@@ -0,0 +1,21 @@
+package com.greedy.section01.javaconfig;
+
+public class Application1 {
+
+	public static void main(String[] args) {
+
+		/* ComponentScan 기능을 이용한 bean등록 설정을 확인 
+		 * 
+		 *  ComponentScan이란?
+		 *  base-package로 설정된 경로 하위에 특정 어노테이션을 가지고 있는 클래스를 이용하여 bean으로 등록한다.
+		 *  @Component annotation이 작성된 클래스를 인식하여 bean으로 만들게 되면,
+		 *  특수 목적에 따라 세부기능을 제공하는
+		 *  @Controller, @Service, @Repository(=DAO), @Configuration(=설정값) 등을 인식한다.
+		 *  */
+		
+		
+		
+		
+	}
+
+}
