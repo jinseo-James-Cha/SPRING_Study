28df273086c3b9e2982f5ee66abfd3b77dd7b059
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-08 11:04:55 +0900

    2021-06-08 5.2.9 spring-context, spring-core jars added

--- chap01-bean-factory-lecture-source/src/com/greedy/section01/xmlconfig/Application.java
+++ chap01-bean-factory-lecture-source/src/com/greedy/section01/xmlconfig/Application.java
@@ -1,5 +1,8 @@
 package com.greedy.section01.xmlconfig;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -10,6 +13,9 @@ public class Application {
 		 * 스프링 컨테이너의 최상위 컨테이너이며, ApplicationContext와 함께 스프링 컨테이너라고 한다.
 		 * Bean의 생성과 설정, 관리 등의 역할을 맡고있다.
 		 *  */
+		
+		ApplicationContext context = new GenericXmlApplicationContext("");
+		
 	}
 
 }
