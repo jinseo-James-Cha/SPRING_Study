dd3c032c1a7a6e1a7b88a865d81094ed53681588
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-08 11:38:17 +0900

    2021-06-08 xmlconfig done

--- chap01-bean-factory-lecture-source/src/com/greedy/section01/xmlconfig/Application.java
+++ chap01-bean-factory-lecture-source/src/com/greedy/section01/xmlconfig/Application.java
@@ -19,6 +19,27 @@ public class Application {
 		ApplicationContext context = new GenericXmlApplicationContext("com/greedy/section01/xmlconfig/spring-context.xml");
 		
 		System.out.println(context);
+		
+		/* 해당 설정값에 들어간 값을 불러오기 -> xml에 있는 값 id로 불러오기
+		 * 아래 예제를 통해 container에 등록한 bean을 읽어오는 방식 3가지에 집중하는 것이다.
+		 *   */
+		
+		/* 1. bean의 id를 이용해서 bean을 가져오는 방법
+		 *  	id를 이용하는 경우 bean의 정확한 type을 유추할 수 없기 대문에 Object타입으로 반환하므로 다운캐스팅하여 사용한다. */
+		MemberDTO member1 = (MemberDTO)context.getBean("member");
+
+		/* 2. Bean의 클래스 메타 정보를 전달하여 가져오는 방법
+		 * 		가져오려는 bean의 타입을 명확히 전달하기 때문에 형변환이 필요없다. */
+		MemberDTO member2 = context.getBean(MemberDTO.class);
+		
+		/* 3. Bean의 id와 클래스 메터정보를 전달하여 가져오는 방법 
+		 * 		가져오려는 bean의 타입을 전달하기 때문에 형변환이 필요없다. */
+		MemberDTO member3 = context.getBean("member", MemberDTO.class);
+
+		
+		System.out.println(member1);
+		System.out.println(member2);
+		System.out.println(member3);
 	}
 
 }
