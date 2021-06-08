581da57e84bd6dac79b57ed09fdf75a10ec42bdc
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-08 10:33:38 +0900

    2021-06-08 Spring study first commit

---
+++ chap01-bean-factory-lecture-source/src/com/greedy/section01/xmlconfig/Application.java
@@ -0,0 +1,15 @@
+package com.greedy.section01.xmlconfig;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		/* IoC 컨테이너라 불리는 존재의 최상위 인터페이스 Bean Factory 확 */
+		
+		/* BeanFactoryfks?
+		 * 스프링 컨테이너의 최상위 컨테이너이며, ApplicationContext와 함께 스프링 컨테이너라고 한다.
+		 * Bean의 생성과 설정, 관리 등의 역할을 맡고있다.
+		 *  */
+	}
+
+}
