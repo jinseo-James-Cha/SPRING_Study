2caa61f176fa8baa578d7dd1f619acc406b25ac4
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-09 15:40:21 +0900

    2021-06-09 qualifier annotation

--- chap05-di-annotation-lecture-source/src/com/greedy/section01/primary/PokemonService.java
+++ chap05-di-annotation-lecture-source/src/com/greedy/section01/primary/PokemonService.java
@@ -1,11 +1,19 @@
 package com.greedy.section01.primary;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PokemonService {
 
+	/* 필드 주입을 이용하는 경우 */
+	
+	/* @Qualifier 어노테이션으로 의존성 주입을 하는 경우 
+	 * 타입이 아닌 클래스이름(첫들자소문자id)로 주입을 한다. 
+	 * @Primary 보다 우선권을 갖는다 */
+//	@Autowired
+//	@Qualifier("squirtle")
 	private Pokemon pokemon;
 	
 	@Autowired
