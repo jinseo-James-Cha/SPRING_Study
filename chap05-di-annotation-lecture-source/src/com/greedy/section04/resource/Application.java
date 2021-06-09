29701bbbec984efa9b24f9124debed5ba1862fe0
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-09 16:30:25 +0900

    2021-06-09 @Resource annotation done

--- chap05-di-annotation-lecture-source/src/com/greedy/section04/resource/Application.java
+++ chap05-di-annotation-lecture-source/src/com/greedy/section04/resource/Application.java
@@ -7,9 +7,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
 	public static void main(String[] args) {
+
+		/*
+		 * 스프링 전용인 @Autowired 대신 
+		 * 자바 표준 어노테이션 @Resource로 POJO 연결
+		 * 
+		 * 스프링이 @Autowired를 처음 선보이고 얼마지나지않아 자바 진영에서도 동일한 기능의
+		 * 어노테이션을 여러 개 표준화했다.
+		 * @Resource와 @Inject, @named
+		 * 
+		 * @Autowired와 @Qualifier와 같은 의미이다.
+		 * 동일 타입의 bean이 여러개 있는 경우, 대상이 모호할 때 대상을 명확하게 한다.
+		 * */
 		
-		/* 세 개의 Bean을 모두 사용해야 하는 경우
-		 * 이 때는 의존성 주입을 받을 필드의 타입을 변경해야한다. */
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.greedy.section04.resource");
 		
--- chap05-di-annotation-lecture-source/src/com/greedy/section04/resource/PokemonService.java
+++ chap05-di-annotation-lecture-source/src/com/greedy/section04/resource/PokemonService.java
@@ -1,45 +1,59 @@
 package com.greedy.section04.resource;
 
-import java.util.Iterator;
-import java.util.Map;
+import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PokemonService {
 	
-	/* 1. List 타입으로 주입 받기 
-		 List 타입으로 하나의 타입인 번들을 한번에 주입할 수 있다. 
-		 순서는 bean의 클래스명 영어 알파벳 사전순이다. */
+	/* 기본적으로 이름으로 주입을 받는다.
+	 * 그리고, bean의 이름이 일치하지 않으면 타입으로 주입받는다.
+	 * 
+	 * 하지만, 동일한 타입의 bean이 있는 경우에는 주입받지 못한다.
+	 * */
+	
+	/* 1. 같은 타입의 bean이 여러개 */
+	@Resource(name="charmander")
+	private Pokemon pokemon;
+	
+	public void pokemonAttack() {
+		pokemon.attack();
+	}
+	
+///////////////////////////////////////////////////////	
+	/* 여러개 중에 원하는 bean만 가져오기 
+	 * @Resource에도 @Qualifier 붙이기 가능.
+	 * */
+//	@Resource
+//	@Qualifier("squirtle")
 //	private List<Pokemon> pokemonList;
-//	
-//	@Autowired
-//	public PokemonService(List<Pokemon> pokemonList) {
-//		this.pokemonList = pokemonList;
-//	}
 //	
 //	public void pokemonAttack() {
 //		for(Pokemon pokemon : pokemonList) {
 //			pokemon.attack();
 //		}
 //	}
+///////////////////////////////////////////////////////	
+	/* 2. 같은 타입의 bean 한 개 일때(Charmander와 Squirtle 두개의 @Component 주석하고 테스트) */
+//	private Pokemon pokemon;
 	
-	/* 2. Map 타입으로 주입 받기 */
-	private Map<String, Pokemon> pokemonMap;
+	/* 2-1 @Resource 는 생성자 주입을 하지 못한다.(애초에 에로발생) */
+//	@Resource
+//	public PokemonService(Pokemon pokemon) {
+//		this.pokemon = pokemon;
+//	}
 	
-	@Autowired
-	public PokemonService(Map<String, Pokemon> pokemonMap) {
-		this.pokemonMap = pokemonMap;
-	}
-	
-	public void pokemonAttack() {
-		Iterator<String> iter = pokemonMap.keySet().iterator();
-		while(iter.hasNext()) {
-			String key = iter.next();
-			System.out.println("pokemonMap의 key : " + key);
-			pokemonMap.get(key).attack();
-		}
-	}
+	/* 2-2 @Resource 는 setter 주입을 할 수 있다.*/
+//	private Pokemon pokemon;
+//	
+//	@Resource
+//	public void setPokemon(Pokemon pokemon) {
+//		this.pokemon = pokemon;
+//	}
+//	
+//	public void pokemonAttack() {
+//		pokemon.attack();
+//	}
 	
 }
