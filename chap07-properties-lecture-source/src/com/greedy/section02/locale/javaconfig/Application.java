151aff0b8aa289aa51a15f16e2a9c1725dede170
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-10 12:49:12 +0900

    2021-06-10 I18N study started

---
+++ chap07-properties-lecture-source/src/com/greedy/section02/locale/javaconfig/Application.java
@@ -0,0 +1,24 @@
+package com.greedy.section02.locale.javaconfig;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * i18n 소프트웨어의 국제화(InternationalizatioN)의 I와 N사이의 알파벳 갯수 18개)
+		 * 소프트웨어를 국제화 하기 위해서는 처리해야할 작업들이 많다.
+		 * 
+		 *  1. 언어, 지역별 번역
+		 *  2. OS/플랫폼 인코딩
+		 *  3. 문자열 치환 방법 : 서버쪽, 클라이언트쪽 치환 중 서버쪽에서 하는게 좋다.
+		 *                    Why? 사용자 디바이스에 리소스 및 렌더링 성능 이슈 발생
+		 *  4. 국제화 UI(문자열 크기 변화, 폰트, 아이콘)
+		 *  5. 쓰기 방향의 차이
+		 *  6. 숫자, 공백, 화폐, 날짜, 주소, 측정단위 등
+		 *  7. 타임존, 섬머타임 등 시각
+		 *  8. 문자열 정렬 방법
+		 * 
+		 *  */
+	}
+
+}
---
+++ chap07-properties-lecture-source/src/com/greedy/section02/locale/javaconfig/config/ContextConfiguration.java
@@ -0,0 +1,8 @@
+package com.greedy.section02.locale.javaconfig.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContextConfiguration {
+
+}
