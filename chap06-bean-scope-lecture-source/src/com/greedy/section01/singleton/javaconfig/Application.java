614177f70721ef4b1eaeb94061bc99c763b36c4e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-10 10:23:44 +0900

    2021-06-10 Bean scope study started

---
+++ chap06-bean-scope-lecture-source/src/com/greedy/section01/singleton/javaconfig/Application.java
@@ -0,0 +1,9 @@
+package com.greedy.section01.singleton.javaconfig;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ chap06-bean-scope-lecture-source/src/com/greedy/section01/singleton/javaconfig/config/ContextConfiguration.java
@@ -0,0 +1,9 @@
+package com.greedy.section01.singleton.javaconfig.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContextConfiguration {
+
+	
+}
