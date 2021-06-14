74d6f41d2bc64e695dbcf3b1b0ab47707f640353
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-14 11:40:14 +0900

    2021-06-14 selectMenuList done

--- chap10-mybatis-spring-lecture-source/src/com/greedy/section01/connection/javaconfig/Application.java
+++ chap10-mybatis-spring-lecture-source/src/com/greedy/section01/connection/javaconfig/Application.java
@@ -1,5 +1,7 @@
 package com.greedy.section01.connection.javaconfig;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,7 +14,15 @@ public class Application {
 		ApplicationContext context = 
 				new AnnotationConfigApplicationContext(ContextConfiguration.class);
 		
+		System.out.println("전체 메뉴 조회하기");
 		
+		//                                        Bean등록에 쓰인 ID, 클래스타입(보통 상속한 인터페이스)
+		MenuService menuService = context.getBean("menuService", MenuService.class);
+	
+		List<MenuDTO> menuList = menuService.selectMenuList();
+		for(MenuDTO menu : menuList) {
+			System.out.println(menu);
+		}
 		
 	}
 
