42bf000c5b41cc43abc723d62d7f37705913a10c
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-06-17 10:18:10 +0900

    2021-06-17 first commit

--- chap25-view-resolver-lecture-source/src/main/java/com/greedy/resolver/ResolverController.java
+++ chap25-view-resolver-lecture-source/src/main/java/com/greedy/resolver/ResolverController.java
@@ -24,6 +24,7 @@ public class ResolverController {
 	public String stringRedirect(Model model) throws UnsupportedEncodingException {
 		
 		model.addAttribute("message", URLEncoder.encode("문자열로 뷰 이름 반환하며 리다이렉트", "UTF-8"));
+
 		
 		/* 접두사로 redirect:을 하면
 		 * forward가 아닌 redirect시킨다 */
